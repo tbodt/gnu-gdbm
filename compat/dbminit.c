50f332ec721165bf092f299bfd5379a7e05f0354
Sergey Poznyakoff <gray@gnu.org.ua>
2011-08-03 13:10:23 +0000

    Update copyright headers.

--- compat/dbminit.c
+++ compat/dbminit.c
@@ -2,21 +2,21 @@
    DBM interface. */
 
 /* This file is part of GDBM, the GNU data base manager.
-   Copyright (C) 1990, 1991, 1993, 2007  Free Software Foundation, Inc.
+   Copyright (C) 1990, 1991, 1993, 2007, 2011 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   GDBM is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
-   This program is distributed in the hope that it will be useful,
+   GDBM is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+   along with GDBM. If not, see <http://www.gnu.org/licenses/>.   */
 
 /* Include system configuration before all else. */
 #include "autoconf.h"
--- src/lock.c
+++ src/lock.c
@@ -1,21 +1,20 @@
 /* lock.c - Implement basic file locking for GDBM. */
 
 /* This file is part of GDBM, the GNU data base manager.
-   Copyright (C) 2008  Free Software Foundation, Inc.
+   Copyright (C) 2008, 2011 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   GDBM is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
-   This program is distributed in the hope that it will be useful,
+   GDBM is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+   along with GDBM. If not, see <http://www.gnu.org/licenses/>.   */
 
 /* Include system configuration before all else. */
 #include "autoconf.h"
